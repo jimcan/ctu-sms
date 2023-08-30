8b88c56df3dcb353c2e56f18f5f5c3e19dcefce5
jimcan <jimcan009@gmail.com>
2023-08-30 17:21:04 +0800

    check

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -33,5 +33,21 @@ export const handle: Handle = async ({ event, resolve }) => {
 	if (secsErr) event.locals.appError = secsErr;
 	if (subsErr) event.locals.appError = subsErr;
 
+	console.error(error);
+	console.error(secsErr);
+	console.error(subsErr);
+	console.log(sections);
+	console.log(subjects);
+	console.log(decodedToken);
+
+	console.log({
+		error,
+		secsErr,
+		subjects,
+		sections,
+		subsErr,
+		decodedToken
+	});
+
 	return await resolve(event);
 };
